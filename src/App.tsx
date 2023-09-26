b58bde48bbde2790711ba5f0a6f3fc6be4347c05
adil2321 <kalikulov71@mail.ru>
2023-09-26 20:08:59 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     Hello, samurai! Let's go!d
+    </div>
+  );
+}
+
+export default App;
