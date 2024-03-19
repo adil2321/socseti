56117d25ec40569ca5c58e7d6fe2b3e9f0fb2810
adil2321 <kalikulov71@mail.ru>
2024-03-19 21:25:01 +0300

    dfsdfsdf

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,10 @@ import {Header} from "./Components/Header/Header";
 import {NavBar} from "./Components/NavBar/NavBar";
 import {Profile} from "./Components/Profile/Profile";
 import {Dialogs} from "./Components/Dialogs/Dialogs";
+import {BrowserRouter, Route} from "react-router-dom";
+import {News} from "./Components/News/News";
+import {Music} from "./Components/Music/Music";
+import {Settings} from "./Components/Settings/Settings";
 
 
 
@@ -12,16 +16,20 @@ function App() {
 
 
     return (
-        <div className="app-wrapper">
-            <Header/>
-            <NavBar/>аааааааааааааааа
-         {/*   <Profile/>*/}
-            <div className={"app-wrapper-content"}>
-               {/* <Dialogs/>*/}
-                <Profile/>
-            </div>
+      <BrowserRouter>
+          <div className="app-wrapper">
+              <Header/>
+              <NavBar/>
+              <div className={"app-wrapper-content"}>
+                  <Route path={'/Dialogs'}  component={Dialogs}/>
+                  <Route path={'/Profile'}  component={Profile}/>
+                  <Route path={'/News'}  component={News}/>
+                  <Route path={'/Music'}  component={Music}/>
+                  <Route path={'/Settings'}  component={Settings}/>
 
-        </div>
+              </div>
+          </div>
+      </BrowserRouter>
     );
 }
 
--- src/Components/NavBar/NavBar.tsx
+++ src/Components/NavBar/NavBar.tsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import classes from './NavBar.module.css'
+import s from './NavBar.module.css'
+import {NavLink} from "react-router-dom";
+
 
 export const NavBar = () => {
     return (
 
 
-
-            <nav className={classes.nav}>
-                <div className={classes.item}><a href={'/Profile'}>Profile</a></div>
-                <div className={`${classes.item} ${classes.Active}`}><a href={'Dialogs'}>Message</a></div>
-                <div className={classes.item}><a href={'News'}>News</a></div>
-                <div className={classes.item}><a href={'Music'}>Music</a></div>
-                <div className={classes.item}><a href={'Settings'}>Settings</a></div>
-            </nav>
+        <nav className={s.nav}>
+            <div className={s.item}><NavLink to={'/Profile'} activeClassName={s.ActiveLink}>Profile</NavLink></div>
+            <div className={`${s.item} ${s.Active}`}><NavLink to={'/Dialogs'} activeClassName={s.ActiveLink}>Message</NavLink></div>
+            <div className={s.item}><NavLink to={'/News'} activeClassName={s.ActiveLink}>News</NavLink></div>
+            <div className={s.item}><NavLink to={'/Music'} activeClassName={s.ActiveLink}>Music</NavLink></div>
+            <div className={s.item}><NavLink to={'/Settings'} activeClassName={s.ActiveLink}>Settings</NavLink></div>
+        </nav>
 
 
     );
 };
+
 
